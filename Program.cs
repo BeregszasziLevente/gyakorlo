b12e21f7fa48a2de3ec817dfccf18a32ad543fa6
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-07 14:27:57 +0200

    kiegészítés

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿
-int szam1 = 1;
-int szam2 = 10;
+int szam1 = 15;
+int szam2 = 15;
 
-if (szam1 < szam2) Console.WriteLine($"A nagyobb szám: {szam2}");
-else if (szam1 < szam2) Console.WriteLine($"A nagyobb szám: {szam1}");
+if (szam1 < szam2)
+{
+    szam2 = szam2 * 2;
+    Console.WriteLine($"A nagyobb szám: {szam2}");
+}
+else if (szam1 < szam2) 
+{
+    szam1 = szam1 * 2;    
+    Console.WriteLine($"A nagyobb szám: {szam1}"); 
+}
 else Console.WriteLine("A két szám egyenlő");
