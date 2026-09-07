97edc7da95f5451f92246a806e8392b5daea5fa0
BeregszasziLevente <beregszaszi.levente.2024d@telekimezotur.hu>
2026-09-07 14:22:58 +0200

    gyakorlás

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿
-Console.Write("Szia!");
-Console.WriteLine("Hello, World!");
-int number = 42;
+int szam1 = 1;
+int szam2 = 10;
+
+if (szam1 < szam2) Console.WriteLine($"A nagyobb szám: {szam2}");
+else if (szam1 < szam2) Console.WriteLine($"A nagyobb szám: {szam1}");
+else Console.WriteLine("A két szám egyenlő");
